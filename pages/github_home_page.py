c905f42a0bc1bec3906c01a189db8bb13879c5a6
Shahar Moshe Refaeli <44533369+ShaharM7@users.noreply.github.com>
2025-12-13 17:17:03 +0200

    update sign-in button locator - change selector from CSS to link text for improved readability - test pass

--- pages/github_home_page.py
+++ pages/github_home_page.py
@@ -7,7 +7,7 @@ from pages.sign_in_page import SignInPage
 
 
 class GitHubHomePage(AbstractPage):
-    SIGN_IN_BUTTON = (By.CSS_SELECTOR, "a[href='/login']")
+    SIGN_IN_BUTTON = (By.LINK_TEXT, "Sign in")
 
     def __init__(self, browser, awaiter: Awaiter, sign_in_page: SignInPage):
         super().__init__(browser, awaiter)
